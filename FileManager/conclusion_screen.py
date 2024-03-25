cde7b987fbe6421bb36083070705100e38b7d54a
SeresP99 <71933391+SeresP99@users.noreply.github.com>
2024-03-25 10:30:58 +0100

    Create(conclusion_screen.py): Add list of files and marked states display

---
+++ FileManager/conclusion_screen.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+from file import File
+import os
+
+
+class ConclusionScreen:
+    def __init__(self, file_array, directory):
+        self.file_array = file_array
+        self.directory = directory
+
+        # self.root = tk.Toplevel()
+        self.root = tk.Tk()
+        self.root.geometry("500x1000")
+        self.root.resizable(True, True)
+
+        # self.root.columnconfigure(0, weight=1)
+        # self.root.rowconfigure(0, weight=1)
+
+        recycle_view_array = []
+        for item in self.file_array:
+            print(str(item))
+            temp_label = tk.Label(self.root, text=str(item))
+            temp_label.pack(padx=10, pady=10, anchor="nw")
+            recycle_view_array.append(temp_label)
+
+        self.root.mainloop()
